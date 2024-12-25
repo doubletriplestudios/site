41864329042935c0c598799671abc6c793f50545
tzudio <contact.doubletriple@gmail.com>
2024-12-25 20:36:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// ฟังก์ชันที่แสดงข้อมูลสมาชิกทีม
-function showTeamMemberInfo(name, position) {
-  // แสดงข้อมูลใน alert
-  alert(`Name: ${name}\nPosition: ${position}`);
-}
 
-// ฟังก์ชันที่นำทางไปยังหน้าอื่น
-function redirectToTeamMemberPage(url) {
-  window.location.href = url;
-}
-
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault(); // ป้องกันการส่งฟอร์มจริง
-  document.getElementById('responseMessage').style.display = 'block';
-});
